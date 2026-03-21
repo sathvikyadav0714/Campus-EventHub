a983dc391ff58cd665c7eefb571c35b3822318db
sathvikyadav0714 <sathvikyadav0714@gmail.com>
2026-03-22 00:57:24 +0530

    fix deployment

--- app.py
+++ app.py
@@ -736,9 +736,8 @@ def generate_certificates(event_id):
 
 
 # ⚠️ KEEP THIS ALWAYS AT THE END
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
