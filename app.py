4497fba49f9eba220494d103dca6c58f073c8729
sathvikyadav0714 <sathvikyadav0714@gmail.com>
2026-04-05 19:34:37 +0530

    add test route

--- app.py
+++ app.py
@@ -753,3 +753,7 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+@app.route("/test")
+def test():
+    return "WORKING"
